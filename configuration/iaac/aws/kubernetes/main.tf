4ff851b2b3fb1a4a20f68ee8fa5cf50e204fa196
Marco Krebs <marco.krebs@t-systems.com>
2020-12-22 14:50:00 +0100

    change cluster version

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -32,7 +32,7 @@ provider "kubernetes" {
 module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.14"
+  cluster_version = "1.18"
   subnets         = ["subnet-a2738593", "subnet-6a83f127"] # CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
